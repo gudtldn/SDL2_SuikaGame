21146a26f895c2ad74e1e79eeae7d51deac56871
gudtldn <gudtldn1234@gmail.com>
2024-10-26 00:14:25 +0900

    버튼 구조체 추가

---
+++ SDL2_SuikaGame/Source/Engine/Button.c
@@ -0,0 +1,42 @@
+#include "Button.h"
+
+Button CreateButton(SDL_Rect rect, SDL_Texture* texture)
+{
+    return (Button) {
+        .rect = rect,
+        .texture = texture,
+        .is_hovered = false,
+        .is_clicked = false
+    };
+}
+
+void UpdateHandleButton(Button* button, void (*callback)())
+{
+    int mouse_x, mouse_y;
+    Uint32 state = SDL_GetMouseState(&mouse_x, &mouse_y);
+
+    button->is_hovered = mouse_x >= button->rect.x && mouse_x <= button->rect.x + button->rect.w &&
+                         mouse_y >= button->rect.y && mouse_y <= button->rect.y + button->rect.h;
+
+    if (button->is_hovered && state & SDL_BUTTON(SDL_BUTTON_LEFT))
+    {
+        button->is_clicked = true;
+        callback();
+    }
+    else
+    {
+        button->is_clicked = false;
+    }
+}
+
+void RenderButton(SDL_Renderer* renderer, Button* button)
+{
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+    SDL_RenderCopy(renderer, button->texture, NULL, &button->rect);
+}
+
+void DestroyButton(Button* button)
+{
+    SDL_DestroyTexture(button->texture);
+    button->texture = NULL;
+}
