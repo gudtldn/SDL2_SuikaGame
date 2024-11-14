6c079ac82602baa6a29f02913ed177b2320d01ee
gudtldn <gudtldn1234@gmail.com>
2024-11-14 21:20:50 +0900

    ♻️ FruitResourceObject.h에서 GetFruitTexture 메서드를 std::array::at()으로 수정하고, GetRandomFruitTexture 메서드에 최소 및 최대 인덱스 매개변수를 추가

--- SDL2_SuikaGame/Source/SuikaGame/GameResources/FruitResourceObject.h
+++ SDL2_SuikaGame/Source/SuikaGame/GameResources/FruitResourceObject.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#include <EnginePCH.h>
+#include "EnginePCH.h"
 #include <array>
 
 
@@ -12,12 +12,26 @@ private:
 public:
     FruitResourceObject(GameEngine* engine);
 
+
     /****** Getters ******/
 
     /// @brief 과일 텍스쳐를 가져옵니다.
     /// @param idx 과일 인덱스 (0 ~ 10)
     /// @return 과일 텍스쳐
     /// @throw std::out_of_range idx가 0보다 작거나 10보다 큰 경우
-    const Texture2D* GetFruitTexture(size_t idx) const { return fruit_textures[idx].get(); }
-    const Texture2D* GetRandomFruitTexture() const { return GetFruitTexture(Math::RandRange(0, 10)); }
+    const Texture2D* GetFruitTexture(size_t idx) const
+    {
+        return fruit_textures.at(idx).get();
+    }
+
+    /// @brief 랜덤한 과일 텍스쳐를 가져옵니다.
+    /// @param min_idx 최소 과일 인덱스 (0 ~ max)
+    /// @param max_idx 최대 과일 인덱스 (min ~ 10)
+    /// @return 과일 텍스쳐
+    /// @throw std::out_of_range min_idx가 0보다 작거나 10보다 큰 경우
+    /// @throw std::out_of_range max_idx가 min_idx보다 작거나 10보다 큰 경우
+    const Texture2D* GetRandomFruitTexture(int min_idx, int max_idx) const
+    {
+        return GetFruitTexture(Math::RandRange(min_idx, max_idx));
+    }
 };
