00130a5f3c3c610c987f21bd521d3868efb8e389
gudtldn <gudtldn1234@gmail.com>
2024-11-14 21:21:22 +0900

    🚧 FruitObject 클래스에 FruitResourceObject를 추가하고, BeginPlay 메서드에서 랜덤 과일 텍스처를 설정하도록 수정

--- SDL2_SuikaGame/Source/SuikaGame/GameObjects/FruitObject.cpp
+++ SDL2_SuikaGame/Source/SuikaGame/GameObjects/FruitObject.cpp
@@ -1,8 +1,31 @@
 ﻿#include "FruitObject.h"
+#include "SuikaGame/GameResources/FruitResourceObject.h"
 
 
 FruitObject::FruitObject(GameEngine* engine)
     : GameObject(engine)
 {
-    
+    std::vector<FruitResourceObject*> fruit_resource;
+    GetCurrentStage()->GetObjectManager().GetGameObjectsOfClass<FruitResourceObject>(fruit_resource);
+
+    THROW_IF_FAILED(
+        !fruit_resource.empty(),
+        "Failed to find FruitResourceObject in the current stage"
+    )
+
+    fruit_resource_object = fruit_resource.front();
+}
+
+void FruitObject::BeginPlay()
+{
+    // 체리, 딸기, 포도, 오렌지
+    fruit_texture = fruit_resource_object->GetRandomFruitTexture(0, 3);
+}
+
+void FruitObject::Update(float delta_time)
+{
+}
+
+void FruitObject::Render(SDL_Renderer* renderer) const
+{
 }
--- SDL2_SuikaGame/Source/SuikaGame/GameObjects/FruitObject.h
+++ SDL2_SuikaGame/Source/SuikaGame/GameObjects/FruitObject.h
@@ -1,12 +1,20 @@
 ﻿#pragma once
 #include "EnginePCH.h"
 
+class FruitResourceObject;
 
+
+/// @brief 과일 오브젝트
 class FruitObject : public GameObject
 {
+    FruitResourceObject* fruit_resource_object;
+    const Texture2D* fruit_texture;
+
 public:
     FruitObject(GameEngine* engine);
 
 protected:
-    
+    virtual void BeginPlay() override;
+    virtual void Update(float delta_time) override;
+    virtual void Render(SDL_Renderer* renderer) const override;
 };
