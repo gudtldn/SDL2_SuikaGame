d7219c6a87c719cb30c4f5ff653b4c4f2dabb51d
gudtldn <gudtldn1234@gmail.com>
2024-11-14 21:32:00 +0900

    ✏️ 과일 텍스처 주석 추가

--- SDL2_SuikaGame/Source/SuikaGame/GameResources/FruitResourceObject.cpp
+++ SDL2_SuikaGame/Source/SuikaGame/GameResources/FruitResourceObject.cpp
@@ -6,17 +6,17 @@ FruitResourceObject::FruitResourceObject(GameEngine* engine)
 {
     // TODO: 나중에 스프라이트 시트로 변경
     const char* fruits_png[] = {
-        "Contents/Textures/fruits/fruit_0.png",
-        "Contents/Textures/fruits/fruit_1.png",
-        "Contents/Textures/fruits/fruit_2.png",
-        "Contents/Textures/fruits/fruit_3.png",
-        "Contents/Textures/fruits/fruit_4.png",
-        "Contents/Textures/fruits/fruit_5.png",
-        "Contents/Textures/fruits/fruit_6.png",
-        "Contents/Textures/fruits/fruit_7.png",
-        "Contents/Textures/fruits/fruit_8.png",
-        "Contents/Textures/fruits/fruit_9.png",
-        "Contents/Textures/fruits/fruit_10.png",
+        "Contents/Textures/fruits/fruit_0.png",  // 체리
+        "Contents/Textures/fruits/fruit_1.png",  // 딸기
+        "Contents/Textures/fruits/fruit_2.png",  // 포도
+        "Contents/Textures/fruits/fruit_3.png",  // 오렌지
+        "Contents/Textures/fruits/fruit_4.png",  // 감
+        "Contents/Textures/fruits/fruit_5.png",  // 사과
+        "Contents/Textures/fruits/fruit_6.png",  // 자몽
+        "Contents/Textures/fruits/fruit_7.png",  // 복숭아
+        "Contents/Textures/fruits/fruit_8.png",  // 파인애플
+        "Contents/Textures/fruits/fruit_9.png",  // 멜론
+        "Contents/Textures/fruits/fruit_10.png", // 수박
     };
 
     for (size_t i = 0; i < fruit_textures.max_size(); ++i)
