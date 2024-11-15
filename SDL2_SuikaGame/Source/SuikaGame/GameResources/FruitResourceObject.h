1a5b42e292ad1359a13ce27d550aa9bd54e23c28
gudtldn <gudtldn1234@gmail.com>
2024-11-15 10:36:34 +0900

    ♻️ 필요없는 메서드 제거

--- SDL2_SuikaGame/Source/SuikaGame/GameResources/FruitResourceObject.h
+++ SDL2_SuikaGame/Source/SuikaGame/GameResources/FruitResourceObject.h
@@ -28,20 +28,6 @@ public:
         return fruit_textures.at(idx).get();
     }
 
-    /// @brief 랜덤한 과일 텍스쳐를 가져옵니다.
-    /// @param min_idx 최소 과일 인덱스 (0 ~ max)
-    /// @param max_idx 최대 과일 인덱스 (min ~ 10)
-    /// @return 과일 텍스쳐
-    /// @throw std::out_of_range min_idx가 0보다 작거나 10보다 큰 경우
-    /// @throw std::out_of_range max_idx가 min_idx보다 작거나 10보다 큰 경우
-    const Texture2D* GetRandomFruitTexture(size_t min_idx, size_t max_idx) const
-    {
-        return GetFruitTexture(Math::RandRange(
-            static_cast<int>(min_idx),
-            static_cast<int>(max_idx)
-        ));
-    }
-
     /// @brief 과일 텍스쳐의 오프셋을 가져옵니다.
     /// @param idx 과일 인덱스 (0 ~ 10)
     /// @return 과일 텍스쳐의 오프셋
