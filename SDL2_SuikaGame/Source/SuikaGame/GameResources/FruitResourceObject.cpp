eafcf8e284c76a61e65c5723fdf0698b51d5dde0
gudtldn <gudtldn1234@gmail.com>
2024-11-15 10:32:25 +0900

    ✨ FruitResourceObject에서 과일 오프셋 위치 및 크기 배열 추가, 텍스처 로드 로직 개선

--- SDL2_SuikaGame/Source/SuikaGame/GameResources/FruitResourceObject.cpp
+++ SDL2_SuikaGame/Source/SuikaGame/GameResources/FruitResourceObject.cpp
@@ -1,11 +1,38 @@
 ﻿#include "FruitResourceObject.h"
+#include <ranges>
 
 
 FruitResourceObject::FruitResourceObject(GameEngine* engine)
     : GameObject(engine)
+    , fruits_offset_position {
+        Vector2D(-1.0f, 4.5f),  // 체리
+        Vector2D(-1.0f, 0.0f),  // 딸기
+        Vector2D(-2.0f, 0.0f),  // 포도
+        Vector2D(-1.0f, 1.0f),  // 오렌지
+        Vector2D(-1.0f, 4.0f),  // 감
+        Vector2D(-1.0f, 1.0f),  // 사과
+        Vector2D(-1.0f, 1.0f),  // 자몽
+        Vector2D(-1.0f, -2.0f), // 복숭아
+        Vector2D(-1.0f, 12.0f), // 파인애플
+        Vector2D(0.0f, 0.0f),   // 멜론
+        Vector2D(0.0f, 0.0f),   // 수박
+    }
+    , fruits_offset_size {
+        -5.0f,  // 체리
+        0.0f,   // 딸기
+        0.0f,   // 포도
+        -4.0f,  // 오렌지
+        -5.0f,  // 감
+        -2.0f,  // 사과
+        -4.0f,  // 자몽
+        -1.0f,  // 복숭아
+        -16.0f, // 파인애플
+        -1.0f,  // 멜론
+        -1.0f,  // 수박
+    }
 {
     // TODO: 나중에 스프라이트 시트로 변경
-    const char* fruits_png[] = {
+    constexpr std::array fruits_png = {
         "Contents/Textures/fruits/fruit_0.png",  // 체리
         "Contents/Textures/fruits/fruit_1.png",  // 딸기
         "Contents/Textures/fruits/fruit_2.png",  // 포도
@@ -19,7 +46,7 @@ FruitResourceObject::FruitResourceObject(GameEngine* engine)
         "Contents/Textures/fruits/fruit_10.png", // 수박
     };
 
-    constexpr float fruit_scales[] = {
+    constexpr std::array fruits_scale = {
         0.175f,  // 체리
         0.2f,    // 딸기
         0.275f,  // 포도
@@ -33,9 +60,9 @@ FruitResourceObject::FruitResourceObject(GameEngine* engine)
         1.0f,    // 수박
     };
 
-    for (size_t i = 0; i < fruit_textures.max_size(); ++i)
+    for (const auto [idx, png] : fruits_png | std::ranges::views::enumerate)
     {
-        SDL_Texture* raw_fruit_texture = IMG_LoadTexture(engine->GetRenderer(), fruits_png[i]);
+        SDL_Texture* raw_fruit_texture = IMG_LoadTexture(engine->GetRenderer(), png);
         THROW_IF_FAILED(
             raw_fruit_texture,
             "Failed to load fruit texture! SDL Error: {}", SDL_GetError()
@@ -44,11 +71,11 @@ FruitResourceObject::FruitResourceObject(GameEngine* engine)
         int w, h;
         SDL_QueryTexture(raw_fruit_texture, nullptr, nullptr, &w, &h);
 
-        fruit_textures[i] = std::make_unique<Texture2D>(
+        fruit_textures[idx] = std::make_unique<Texture2D>(
             raw_fruit_texture,
             Vector2D(
-                static_cast<float>(w) * fruit_scales[i],
-                static_cast<float>(h) * fruit_scales[i]
+                static_cast<float>(w) * fruits_scale.at(idx),
+                static_cast<float>(h) * fruits_scale.at(idx)
             )
         );
     }
--- SDL2_SuikaGame/Source/SuikaGame/GameResources/FruitResourceObject.h
+++ SDL2_SuikaGame/Source/SuikaGame/GameResources/FruitResourceObject.h
@@ -7,7 +7,11 @@
 class FruitResourceObject : public GameObject
 {
 private:
-    std::array<std::unique_ptr<Texture2D>, 11> fruit_textures;
+    static constexpr size_t FRUIT_COUNT = 11;
+
+    std::array<std::unique_ptr<Texture2D>, FRUIT_COUNT> fruit_textures;
+    std::array<Vector2D, FRUIT_COUNT> fruits_offset_position;
+    std::array<float, FRUIT_COUNT> fruits_offset_size;
 
 public:
     FruitResourceObject(GameEngine* engine);
@@ -30,8 +34,30 @@ public:
     /// @return 과일 텍스쳐
     /// @throw std::out_of_range min_idx가 0보다 작거나 10보다 큰 경우
     /// @throw std::out_of_range max_idx가 min_idx보다 작거나 10보다 큰 경우
-    const Texture2D* GetRandomFruitTexture(int min_idx, int max_idx) const
+    const Texture2D* GetRandomFruitTexture(size_t min_idx, size_t max_idx) const
     {
-        return GetFruitTexture(Math::RandRange(min_idx, max_idx));
+        return GetFruitTexture(Math::RandRange(
+            static_cast<int>(min_idx),
+            static_cast<int>(max_idx)
+        ));
     }
+
+    /// @brief 과일 텍스쳐의 오프셋을 가져옵니다.
+    /// @param idx 과일 인덱스 (0 ~ 10)
+    /// @return 과일 텍스쳐의 오프셋
+    /// @throw std::out_of_range idx가 0보다 작거나 10보다 큰 경우
+    Vector2D GetFruitOffsetPosition(size_t idx) const
+    {
+        return fruits_offset_position.at(idx);
+    }
+
+    /// @brief 과일 텍스쳐의 오프셋을 가져옵니다.
+    /// @param idx 과일 인덱스 (0 ~ 10)
+    /// @return 과일 텍스쳐의 오프셋
+    /// @throw std::out_of_range idx가 0보다 작거나 10보다 큰 경우
+    float GetFruitOffsetSize(size_t idx) const
+    {
+        return fruits_offset_size.at(idx);
+    }
+
 };
