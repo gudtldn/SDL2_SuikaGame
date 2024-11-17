e0640e3bc42e59c689c18127dedab550a9fecc87
gudtldn <gudtldn1234@gmail.com>
2024-11-17 21:03:14 +0900

    ✨ GameStage에 게임오버 관련 변수 추가

--- SDL2_SuikaGame/Source/SuikaGame/Stages/GameStage.h
+++ SDL2_SuikaGame/Source/SuikaGame/Stages/GameStage.h
@@ -9,11 +9,20 @@ private:
     /// @brief 물리 엔진 관리자
     Box2DManager box2d_manager;
 
+    /// @brief 게임 오버 여부
+    bool is_game_over;
+
 public:
     GameStage(GameEngine* engine);
 
     /// @brief 물리 엔진 관리자를 가져옵니다.
     Box2DManager& GetBox2DManager() { return box2d_manager; }
+
+    /// @brief 게임 오버 여부를 반환합니다.
+    bool IsGameOver() const { return is_game_over; }
+
+    /// @brief 게임 오버 여부를 설정합니다.
+    void SetGameOver(bool new_game_over) { is_game_over = new_game_over; }
 
 protected:
     virtual void InitializeObjects() override;
