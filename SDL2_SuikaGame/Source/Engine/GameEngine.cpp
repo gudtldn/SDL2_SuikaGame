3fc1f2a3b73d1472112ee718753ae2b7e652cd15
gudtldn <gudtldn1234@gmail.com>
2024-11-18 00:43:42 +0900

    ✨ 게임 엔진의 이벤트 처리 개선: 게임 종료 및 게임패드 연결/해제 이벤트 추가, 로그 매크로 사용

--- SDL2_SuikaGame/Source/Engine/GameEngine.cpp
+++ SDL2_SuikaGame/Source/Engine/GameEngine.cpp
@@ -2,7 +2,8 @@
 #include "Engine/Components/Stage.h"
 #include "Engine/Components/GameObject.h"
 #include "Engine/Utils/Math.h"
-#include "GlobalMacro.h"
+#include "Engine/Utils/LogMacros.h"
+#include "Engine/GlobalMacro.h"
 
 #include <ctime>
 #include <vector>
@@ -26,7 +27,7 @@ GameEngine::GameEngine(
     , fixed_time_v(1.0f / TARGET_FPS)
 {
     // SDL 미 초기화 시 예외 처리
-    if (SDL_WasInit(SDL_INIT_VIDEO) == 0)
+    if (SDL_WasInit(SDL_INIT_VIDEO) == 0) // TODO: SDL_Manager를 Engine에서 초기화
     {
         throw std::runtime_error("SDL is not initialized.");
     }
@@ -54,6 +55,7 @@ GameEngine::GameEngine(
     if (SDL_NumJoysticks() > 0)
     {
         controller = SDL_GameControllerOpen(0);
+        LOG_INFO("Connected to GameController");
     }
 }
 
@@ -108,10 +110,6 @@ void GameEngine::Run()
         {
             // 이벤트 처리
             HandleEvent(event);
-            if (event.type == SDL_QUIT)
-            {
-                is_running = false;
-            }
         }
 
 
@@ -159,6 +157,46 @@ inline void GameEngine::HandleEvent(const SDL_Event& event)
     {
         current_stage->HandleEvent(event);
     }
+
+
+    // Engine Handle Event
+    switch (event.type)
+    {
+
+    // 창 닫기 이벤트
+    case SDL_QUIT:
+    {
+        is_running = false;
+        break;
+    }
+
+    // 게임패드 연결 이벤트
+    case SDL_CONTROLLERDEVICEADDED:
+    {
+        if (!controller)
+        {
+            controller = SDL_GameControllerOpen(event.cdevice.which);
+            LOG_INFO("Connected to GameController");
+        }
+        break;
+    }
+
+    // 게임패드 해제 이벤트
+    case SDL_CONTROLLERDEVICEREMOVED:
+    {
+        if (controller)
+        {
+            SDL_GameControllerClose(controller);
+            controller = nullptr;
+            LOG_INFO("Disconnected from GameController");
+        }
+        break;
+    }
+
+    // 나머지는 무시
+    default:
+        break;
+    } // switch (event.type)
 }
 
 inline void GameEngine::Update(float delta_time)
@@ -193,6 +231,7 @@ inline void GameEngine::Update(float delta_time)
     }
 }
 
+// ReSharper disable once CppMemberFunctionMayBeConst
 inline void GameEngine::FixedUpdate(float fixed_time)
 {
     if (current_stage)
