09cc6fe04a0edd72a5efe863696b8d6195609500
gudtldn <gudtldn1234@gmail.com>
2024-11-18 18:56:44 +0900

    ♻️ 배경 객체의 색상 초기화 방식 개선 및 코드 정리

--- SDL2_SuikaGame/Source/SuikaGame/GameObjects/BackgroundObject.cpp
+++ SDL2_SuikaGame/Source/SuikaGame/GameObjects/BackgroundObject.cpp
@@ -3,14 +3,14 @@
 
 BackgroundObject::BackgroundObject(GameEngine* engine)
     : GameObject(engine)
-    , background_color({ 0xDD, 0x8E, 0x40, 0xFF })
+    , background_color({ .r = 0xDD, .g = 0x8E, .b = 0x40, .a = 0xFF })
     , second_background_rect({
         .x = 0,
-        .y = SCREEN_HEIGHT / 2 + static_cast<int>((SCREEN_HEIGHT * 0.1)),
+        .y = SCREEN_HEIGHT / 2 + static_cast<int>(SCREEN_HEIGHT * 0.1),
         .w = SCREEN_WIDTH,
         .h = SCREEN_HEIGHT / 2,
     })
-    , second_background_color({0xFF, 0xFE, 0xAA, 0xFF})
+    , second_background_color({.r = 0xFF, .g = 0xFE, .b = 0xAA, .a = 0xFF})
 {
     // 렌더링 순서 (배경은 가장 뒤에 렌더링)
     z_order = -100;
