8f017a069db4a3763126d35b243661853d4058d1
gudtldn <gudtldn1234@gmail.com>
2024-11-25 15:32:10 +0900

    📝 GameOverStage.cpp에 점수 및 최고점수 표시, 버튼 추가를 위한 TODO 주석 추가

--- SDL2_SuikaGame/Source/SuikaGame/Stages/GameOverStage.cpp
+++ SDL2_SuikaGame/Source/SuikaGame/Stages/GameOverStage.cpp
@@ -6,4 +6,9 @@ void GameOverStage::InitializeStage()
 {
     // 게임오버 전, 마지막 스크린샷 화면을 표시하는 오브젝트
     GetObjectManager().CreateGameObject<GameOverScreenCaptureDisplayObject>();
+
+    // TODO: 점수 표시하기
+    // TODO: 최고점수 표시하기
+
+    // TODO: 다시하기 및 메뉴로 돌아가기 버튼 만들기
 }
